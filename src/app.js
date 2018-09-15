95c9c3f42475bf6a7f10169326afb5b04fcf0398
Euan <thecableton@gmail.com>
2018-09-15 19:45:44 +0100

    Attempted to always have datepicker display current date, doesn't work

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const SelectView = require ('./views/select_view');
 const PictureView = require ('./views/picture_view');
 
 document.addEventListener('DOMContentLoaded', () => {
-    const selectElement = document.querySelector('.datepicker');
+    document.window.alert(formatNow());
+    const selectElement = document.querySelector('#datepicker');
     const selectView = new SelectView(selectElement);
+    selectElement.max = formatNow();
     selectView.bindEvents();
     
     const infoParagraph = document.querySelector('#info-paragraph')
@@ -15,17 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
     astronomyPictures.bindEvents();
     astronomyPictures.getData();
 
-    const formatNow = function() {
-        const d = new Date();
-        const year = d.getFullYear();
-    
-        let month = '' + (d.getMonth() + 1);
-        let day = '' + d.getDate();
-    
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-    
-        return [year, month, day].join('-');
-    }
-    
-})
+});
+
+const formatNow = function() {
+    const d = new Date();
+    const year = d.getFullYear();
+
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+};
